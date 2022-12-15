c53de7727d939107b38b9262594f9537f85dd19d
Jeganmuthaiah <jeganmuthaiah@gmail.com>
2022-12-15 23:26:12 +0530

    Multiple Api calls

--- src/main/app/feed/feed.component.ts
+++ src/main/app/feed/feed.component.ts
@@ -15,13 +15,50 @@ export class FeedComponent implements OnInit {
     //   console.log(status);
     // })
 
-    fetch('https://java-httpnew.azurewebsites.net/api/status', {mode: 'no-cors'}).then((response) => {
-      console.log(response);
+    this.makeHttp2Calls();
+    this.makeHttp1Calls();
+
+  }
+
+
+  makeHttp2Calls() {
+    const urls = [ 
+      'https://java-httpnew.azurewebsites.net/api/status',
+      'https://java-httpnew.azurewebsites.net/api/get?wc=1000000',
+      // 'https://java-httpnew.azurewebsites.net/api/status',
+      // 'https://java-httpnew.azurewebsites.net/api/status',
+      // 'https://java-httpnew.azurewebsites.net/api/status',
+      // 'https://java-httpnew.azurewebsites.net/api/status'
+    ]
+
+    const requests = urls.map((url) => fetch(url, {mode: 'no-cors'}));
+    // fetch('https://java-httpnew.azurewebsites.net/api/status', {mode: 'no-cors'}).then((response) => {
+    //   console.log(response);
+    // })
+    Promise.all(requests)
+    .then((responses) => {
+      responses.map((response) => response.json()).map((res) => console.log(res));
     })
+  }
 
 
-    fetch('https://java-httpold.azurewebsites.net/api/status', {mode: 'no-cors'}).then((response) => {
-      console.log(response);
+  makeHttp1Calls() {
+    const urls = [ 
+      'https://java-httpold.azurewebsites.net/api/status',
+      'https://java-httpold.azurewebsites.net/api/get?wc=1000000',
+      // 'https://java-httpold.azurewebsites.net/api/status',
+      // 'https://java-httpold.azurewebsites.net/api/status',
+      // 'https://java-httpold.azurewebsites.net/api/status',
+      // 'https://java-httpold.azurewebsites.net/api/status'
+    ]
+
+    const requests = urls.map((url) => fetch(url, {mode: 'no-cors'}));
+    // fetch('https://java-httpold.azurewebsites.net/api/status', {mode: 'no-cors'}).then((response) => {
+    //   console.log(response);
+    // })
+    Promise.all(requests)
+    .then((responses) => {
+      responses.map((response) => response.json()).map((res) => console.log(res));
     })
   }
 
