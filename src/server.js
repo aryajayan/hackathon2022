4344be59a2a90a70cb294ca9d67c949910d5888d
morganpoc2022 <120382020+morganpoc2022@users.noreply.github.com>
2023-04-13 06:28:55 +0000

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const indexPath = path.resolve(
+  __dirname,
+  "dist/hackathon-http2/index.html"
+);
+
+// static resources should just be served as they are
+app.get(
+  "*.*",
+  express.static(path.resolve(__dirname, "dist/hackathon-http2"))
+);
+
+// here we replace the tags in the index.html page and send them
+app.all("*", function (req, res) {
+  fs.readFile(indexPath, "utf8", (err, htmlData) => {
+    if (err) {
+      console.error("Error during file reading", err);
+      return res.status(404).end();
+    }
+    // get post info
+    const post = {
+      title: "testTitle",
+      description: "testDescription",
+    };
+    // inject meta tags
+    htmlData = htmlData
+      .replace("__META_OG_TITLE__", post.title)
+      .replace("__META_OG_DESCRIPTION__", post.description);
+    return res.send(htmlData);
+  });
+  //test
+});
+// listening...
+app.listen(PORT, (error) => {
+  if (error) {
+    return console.log("Error during app startup", error);
+  }
+  console.log("listening on " + PORT + "...");
+});
